7f396a194a54c6d7840fc2a6639512e68d14f731
Anastasiia Mokrousova <156221286+anamok23@users.noreply.github.com>
2024-02-20 16:09:17 +0100

    Add files via upload
    The project aims to analyze the category "Pens, Pencils, and Writing Supplies" on Amazon using SQL to identify the most popular manufacturers. This will be achieved by examining the average number of reviews for products within this category. The analysis will provide insights into consumer preferences and help identify key players in the market.

---
+++ SQLQuery_Amazon_Pens, Pencils.sql
@@ -0,0 +1,33 @@
+﻿
+--Let us examine the data frame.
+
+SELECT *
+FROM dbo.Amazon
+
+-- Updating the data type of the column 'number_of_reviews' to integer will allow us to order and analyze the data.
+
+UPDATE dbo.Amazon
+SET number_of_reviews = CAST(REPLACE(number_of_reviews, ',', '') AS INT)
+WHERE number_of_reviews LIKE '%,%'
+
+ALTER TABLE dbo.Amazon
+ALTER COLUMN number_of_reviews INT;
+
+-- Let's check which categories are present in the data frame.
+
+SELECT DISTINCT amazon_category_and_sub_category
+FROM dbo.Amazon
+
+-- Separate category and subcategory into separate columns.
+
+SELECT DISTINCT SUBSTRING(amazon_category_and_sub_category, 1, CHARINDEX(' > ' , amazon_category_and_sub_category) -1) AS Category
+FROM dbo.Amazon
+ORDER BY Category
+
+-- We want to analyze the category 'Pens, Pencils, and Writing Supplies' on Amazon to determine the most popular manufacturers based on the average number of reviews.
+
+SELECT manufacturer, AVG(number_of_reviews) AS average_number_of_reviews
+FROM dbo.Amazon
+WHERE amazon_category_and_sub_category LIKE 'Pens%'
+GROUP BY manufacturer
+
